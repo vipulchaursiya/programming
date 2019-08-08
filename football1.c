222a4d01b0a6d25d3d25c396b3a2ab53394ae0a2
vipulchaursiya <vipulchaursiya@gmail.com>
2019-08-08 14:30:18 +0530

    according codechef football1

--- football1.c
+++ football1.c
@@ -4,31 +4,30 @@ int main ()
 {
 	int n,i,j,k, max,g[n],f[n],res[20];
 	int testc=0;
-	printf("enter the number of test cases:");
+	printf("\n");
 	scanf("%d",&testc);
 	for(k=0;k<testc;k++)
 	{
-		printf("\n\n for testcase %d==\n\n",k+1);
-		printf("enter the numbers of player:");
+			printf("\n");
 	scanf("%d",&n);
-	printf("\nenter the goals repspect to th player:");
+	printf("\n");
 	for(i=0;i<n;i++)
 	{
 		scanf("%d",&g[i]);
 	}
-	printf("\nenter the number of fouls respect to the player:");
+	printf("\n");
 	for(i=0;i<n;i++)
 	{
 		scanf("%d",&f[i]);
 	}
-	printf("\ntotal points of player for testcase %d",k+1);
+//	printf("\ntotal points of player for testcase %d",k+1);
 	j=0;
 	for(i=0;i<n;i++)
 	{
 		res[j]=(g[i]*20)-(f[i]*10);
 		if(res[j]<0)
 	    res[j]=0;
-		printf("\ntotal points of player %d is = %d",i+1,res[j]);
+	//	printf("\ntotal points of player %d is = %d",i+1,res[j]);
 		j++; 
 	}
 	max=0;
@@ -41,7 +40,7 @@ int main ()
 		res[max]=res[j];
 		}
 	}
-	printf("\nplayer %d have maximum points:%d",max+1,res[max]);
+	printf("\n%d",res[max]);
 	
    }
 }
